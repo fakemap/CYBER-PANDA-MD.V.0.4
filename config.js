461b97531b540e8400e18d4aecf4693feb2c39aa
fakemap <130793375+fakemap@users.noreply.github.com>
2024-02-13 23:08:51 +0000

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ global.antitags = true;
 global.autoTyping = true;                //auto tying by default off.
 global.autoRecord = false;                //auto recording by default off.
 global.groupevent = false;                //This is the new variable for controlling group event handling.
-global.statusseen = false;                 //This is the new variable for controlling status seen.
+global.statusseen = true;                 //This is the new variable for controlling status seen.
 global.autoreadgc = false;
 
 
